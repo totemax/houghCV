4f8f28350c8b5fe9cca49677ed5cc4f410176dc4
tote <tote@totux>
2015-01-02 19:23:12 +0100

    Finalización de algoritmo de hough con circulo

--- houghCV.py
+++ houghCV.py
@@ -3,13 +3,52 @@
 import cv2
 import numpy as np
 
+def houghCircleTransform(img):
+  "Cálculo de la tranformada de hough para una circunferencia"
+  height = img.shape[0]
+  width = img.shape[1]
+  resultsMatrix = np.zeros([width, height, max(width, height)], np.uint8)
+  whites = np.where(img > 0)
+  for pixel in range(0,len(whites[0])):
+    for i in range(0, width):
+      for j in range(0, height):
+	r = (((whites[0][pixel] - j)**2) + ((whites[1][pixel] - i)**2))**0.5
+	resultsMatrix[i, j, r] += 1
+  print len(np.where(resultsMatrix > 0)[0])
+  
+  maximo = (0,0,0,0)
+  posibles = np.where(resultsMatrix > 0)
+  for circle in range(0, len(posibles[0])):
+    if maximo[0] < resultsMatrix[posibles[0][circle], posibles[1][circle], posibles[2][circle]]:
+      maximo = (resultsMatrix[posibles[0][circle], posibles[1][circle], posibles[2][circle]], posibles[0][circle], posibles[1][circle], posibles[2][circle])
+    elif maximo[0] == resultsMatrix[posibles[0][circle], posibles[1][circle], posibles[2][circle]] and maximo[3] < posibles[2][circle]:
+      maximo = (resultsMatrix[posibles[0][circle], posibles[1][circle], posibles[2][circle]], posibles[0][circle], posibles[1][circle], posibles[2][circle])
+  return maximo
+  
+
+
 # Create a black image
-img = np.zeros((512,512,3), np.uint8)
+img = np.zeros((50,50, 3), np.uint8)
 
-cv2.circle(img,(256,256), 128, (255,255,255), -1)
+cv2.circle(img,(25,25), 10, (255,255,255), -1)
 
+
+
+mask = np.ones([3,3], np.uint8)
+erode = cv2.erode(img, mask, iterations = 1)
+img2 = img - erode;
+
+cv2.cvtColor( img2, cv2.COLOR_BGR2GRAY);
+
+circle = houghCircleTransform(img2)
+
+cv2.circle(img, (circle[1], circle[2]), circle[3] + 2, (0,255,0), 1)
+
+cv2.imshow('borde', img2)
 cv2.imshow('image',img)
+
 k = cv2.waitKey(0) & 0xFF
 while k != ord('q'):
     k = cv2.waitKey(0) & 0xFF
 cv2.destroyAllWindows()
+
